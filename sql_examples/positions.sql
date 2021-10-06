0e6646e3f44c1ea0ab98ba5373d065df670daceb
scrubi19 <scrubi.memo@yandex.ru>
2021-10-06 16:26:30 +0700

    maven's generated default project

---
+++ sql_examples/positions.sql
@@ -0,0 +1,6 @@
+insert into positions values
+(1,'Cтажер', false),
+(2,'Junior Java разработчик', true),
+(3,'Middle Java разработчик', true),
+(4,'Senior Java разработчик', true),
+(5,'Менеджер', false);
